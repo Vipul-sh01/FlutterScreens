e9b21c40c1c620ed2a434b7da8d8c29c9f5f1588
Vipul sharma <vipulsharma6021@gmail.com>
2025-07-05 21:22:10 +0530

    Refactor LoginScreen1: Remove unnecessary 'new' keyword and update button styles to ElevatedButton and TextButton

--- lib/login_screen_1.dart
+++ lib/login_screen_1.dart
@@ -11,42 +11,46 @@ class LoginScreen1 extends StatelessWidget {
     this.primaryColor = Colors.green,
     this.backgroundColor = Colors.white,
     this.backgroundImage = const AssetImage("assets/images/full-bloom.png"),
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: new Container(
+        child: Container(
           height: MediaQuery.of(context).size.height,
           decoration: BoxDecoration(
             color: this.backgroundColor,
+            image: DecorationImage(
+              image: this.backgroundImage,
+              fit: BoxFit.cover,
+            ),
           ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisSize: MainAxisSize.max,
             children: <Widget>[
               Expanded(
-                child: new ClipPath(
+                child: ClipPath(
                   clipper: MyClipper(),
                   child: Container(
-                    decoration: BoxDecoration(
-                      image: new DecorationImage(
-                        image: this.backgroundImage,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
                     alignment: Alignment.center,
                     padding: EdgeInsets.only(top: 100.0, bottom: 100.0),
                     child: Column(
                       children: <Widget>[
                         Text(
                           "DEMO",
-                          style: TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold, color: this.primaryColor),
+                          style: TextStyle(
+                              fontSize: 50.0,
+                              fontWeight: FontWeight.bold,
+                              color: this.primaryColor),
                         ),
                         Text(
                           "Login Screen 1",
-                          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: this.primaryColor),
+                          style: TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                              color: this.primaryColor),
                         ),
                       ],
                     ),
@@ -71,7 +75,7 @@ class LoginScreen1 extends StatelessWidget {
                 margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
                 child: Row(
                   children: <Widget>[
-                    new Padding(
+                    Padding(
                       padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
                       child: Icon(
                         Icons.person_outline,
@@ -84,7 +88,7 @@ class LoginScreen1 extends StatelessWidget {
                       color: Colors.grey.withOpacity(0.5),
                       margin: const EdgeInsets.only(left: 00.0, right: 10.0),
                     ),
-                    new Expanded(
+                    Expanded(
                       child: TextField(
                         decoration: InputDecoration(
                           border: InputBorder.none,
@@ -92,7 +96,7 @@ class LoginScreen1 extends StatelessWidget {
                           hintStyle: TextStyle(color: Colors.grey),
                         ),
                       ),
-                    )
+                    ),
                   ],
                 ),
               ),
@@ -114,7 +118,7 @@ class LoginScreen1 extends StatelessWidget {
                 margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
                 child: Row(
                   children: <Widget>[
-                    new Padding(
+                    Padding(
                       padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
                       child: Icon(
                         Icons.lock_open,
@@ -127,7 +131,7 @@ class LoginScreen1 extends StatelessWidget {
                       color: Colors.grey.withOpacity(0.5),
                       margin: const EdgeInsets.only(left: 00.0, right: 10.0),
                     ),
-                    new Expanded(
+                    Expanded(
                       child: TextField(
                         decoration: InputDecoration(
                           border: InputBorder.none,
@@ -135,51 +139,48 @@ class LoginScreen1 extends StatelessWidget {
                           hintStyle: TextStyle(color: Colors.grey),
                         ),
                       ),
-                    )
+                    ),
                   ],
                 ),
               ),
               Container(
                 margin: const EdgeInsets.only(top: 20.0),
                 padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-                child: new Row(
+                child: Row(
                   children: <Widget>[
-                    new Expanded(
-                      child: FlatButton(
-                        shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
-                        splashColor: this.primaryColor,
-                        color: this.primaryColor,
-                        child: new Row(
+                    Expanded(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0)),
+                          primary: this.primaryColor, // background color
+                          onPrimary: Colors.white, // text color
+                        ),
+                        onPressed: () {
+                          // TODO: Implement login functionality
+                        },
+                        child: Row(
                           children: <Widget>[
-                            new Padding(
+                            Padding(
                               padding: const EdgeInsets.only(left: 20.0),
                               child: Text(
                                 "LOGIN",
                                 style: TextStyle(color: Colors.white),
                               ),
                             ),
-                            new Expanded(
-                              child: Container(),
-                            ),
-                            new Transform.translate(
+                            Expanded(child: Container()),
+                            Transform.translate(
                               offset: Offset(15.0, 0.0),
-                              child: new Container(
+                              child: Container(
                                 padding: const EdgeInsets.all(5.0),
-                                child: FlatButton(
-                                  shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(28.0)),
-                                  splashColor: Colors.white,
-                                  color: Colors.white,
-                                  child: Icon(
-                                    Icons.arrow_forward,
-                                    color: this.primaryColor,
-                                  ),
-                                  onPressed: () => {},
+                                child: Icon(
+                                  Icons.arrow_forward,
+                                  color: this.primaryColor,
                                 ),
                               ),
-                            )
+                            ),
                           ],
                         ),
-                        onPressed: () => {},
                       ),
                     ),
                   ],
@@ -188,44 +189,41 @@ class LoginScreen1 extends StatelessWidget {
               Container(
                 margin: const EdgeInsets.only(top: 10.0),
                 padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-                child: new Row(
+                child: Row(
                   children: <Widget>[
-                    new Expanded(
-                      child: FlatButton(
-                        shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
-                        splashColor: Color(0xFF3B5998),
-                        color: Color(0xff3B5998),
-                        child: new Row(
+                    Expanded(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0)),
+                          primary: Color(0xff3B5998), // Facebook blue color
+                          onPrimary: Colors.white,
+                        ),
+                        onPressed: () {
+                          // TODO: Implement Facebook login functionality
+                        },
+                        child: Row(
                           children: <Widget>[
-                            new Padding(
+                            Padding(
                               padding: const EdgeInsets.only(left: 20.0),
                               child: Text(
                                 "LOGIN WITH FACEBOOK",
                                 style: TextStyle(color: Colors.white),
                               ),
                             ),
-                            new Expanded(
-                              child: Container(),
-                            ),
-                            new Transform.translate(
+                            Expanded(child: Container()),
+                            Transform.translate(
                               offset: Offset(15.0, 0.0),
-                              child: new Container(
+                              child: Container(
                                 padding: const EdgeInsets.all(5.0),
-                                child: FlatButton(
-                                  shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(28.0)),
-                                  splashColor: Colors.white,
-                                  color: Colors.white,
-                                  child: Icon(
-                                    const IconData(0xea90, fontFamily: 'icomoon'),
-                                    color: Color(0xff3b5998),
-                                  ),
-                                  onPressed: () => {},
+                                child: Icon(
+                                  Icons.facebook,
+                                  color: Color(0xff3b5998),
                                 ),
                               ),
-                            )
+                            ),
                           ],
                         ),
-                        onPressed: () => {},
                       ),
                     ),
                   ],
@@ -234,21 +232,20 @@ class LoginScreen1 extends StatelessWidget {
               Container(
                 margin: const EdgeInsets.only(top: 20.0),
                 padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-                child: new Row(
+                child: Row(
                   children: <Widget>[
-                    new Expanded(
-                      child: FlatButton(
-                        shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
-                        color: Colors.transparent,
-                        child: Container(
-                          padding: const EdgeInsets.only(left: 20.0),
-                          alignment: Alignment.center,
-                          child: Text(
-                            "DON'T HAVE AN ACCOUNT?",
-                            style: TextStyle(color: this.primaryColor),
-                          ),
+                    Expanded(
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          primary: this.primaryColor,
                         ),
-                        onPressed: () => {},
+                        onPressed: () {
+                          // TODO: Navigate to Sign Up screen
+                        },
+                        child: Text(
+                          "DON'T HAVE AN ACCOUNT?",
+                          style: TextStyle(color: this.primaryColor),
+                        ),
                       ),
                     ),
                   ],
